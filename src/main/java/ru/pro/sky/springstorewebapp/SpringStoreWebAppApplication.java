f90c3b206d1748eb397e4555e07930a79eb4012d
ad_ho <ad_hoc90@mail.ru>
2023-07-30 16:50:00 +0300

    initial commit

---
+++ src/main/java/ru/pro/sky/springstorewebapp/SpringStoreWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.pro.sky.springstorewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStoreWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStoreWebAppApplication.class, args);
+    }
+
+}
